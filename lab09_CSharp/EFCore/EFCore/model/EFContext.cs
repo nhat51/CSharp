3ddce661f2d179d7809e87fd72ef6397b5dc7d75
nhat51 <65001571+nhat51@users.noreply.github.com>
2021-01-21 12:56:46 +0700

    lab09

---
+++ lab09_CSharp/EFCore/EFCore/model/EFContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.model
+{
+    public class EFContext : DbContext
+    {
+
+        private const string connectionString = "Server=(localdb)\\mssqllocaldb;Database=EFCore;Trusted_Connection=True;";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
